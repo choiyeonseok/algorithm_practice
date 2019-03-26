ae00bcf3fbf033fa0f446f9a6ebfd99af9e047a4
choiyeonseok <smr603@snu.ac.kr>
2019-03-26 10:42:52 +0900

    다음순열업뎃

--- Next_Permutation.py
+++ Next_Permutation.py
@@ -1,27 +1,28 @@
-def flip(some_list):
-    if len(some_list) == 1 or len(some_list) == 0:
-        return some_list
-    return some_list[-1:] + flip(some_list[:-1])
+def swap(list, a, b):
+    temp = list[b]
+    list[b] = list[a]
+    list[a] = temp
 
 
-def next_permutation(clist):
-    cand = 0
-    for i in range(1, len(clist)):
-        #r은 5 ~ 1까지 참조
-        r = len(clist) - i
-        if clist[r - 1] < clist[r]:
-            for j in range(r, len(clist)):
-                if clist[r - 1] > clist[j]:
-                    cand = min(clist[r:j])
-            for j in range(r, len(clist)):
-                if clist[j] == cand:
-                    temp = clist[j]
-                    clist[j] = clist[r-1]
-                    clist[r-1] = temp
-            clist[r:] = flip(clist[r:])
-        if cand != 0:
-            return clist
+def next_permutation(c_list, n):
+    i = n - 1
+    while c_list[i - 1] >= c_list[i]:
+        i -= 1
+    if i <= 0: return False
+    j = n - 1
+    while c_list[j] <= c_list[i - 1]:
+        j -= 1
+    swap(c_list, j, i - 1)
+    j = n - 1
+    while i < j:
+        swap(c_list, j, i)
+        i += 1
+        j -= 1
+    return c_list
 
-print(next_permutation([7, 2, 3, 5, 4, 1]))
+
+c_list = [7, 2, 3, 6, 5, 4, 1]
+n = len(c_list)
+print(next_permutation(c_list, n))
 
 
